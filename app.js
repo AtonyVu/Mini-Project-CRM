a44dac33afcf410f64afd6893c5c441fb51fdc31
luancauthu2907 <luancauthu@gmail.com>
2021-08-04 08:42:43 +0700

    update deploy

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
